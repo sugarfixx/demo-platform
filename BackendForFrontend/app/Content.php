eca59ab9bbce06e8484ac80e33842466d77238db
sugarfixx <ingar.torsrud@me.com>
2021-06-07 08:51:50 +0200

    Add models

---
+++ BackendForFrontend/app/Content.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 07/06/2021
+ * Time: 08:49
+ */
+
+namespace App\Providers;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Content extends Model
+{
+
+}
---
+++ BackendForFrontend/app/Tenant.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 07/06/2021
+ * Time: 08:47
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tenant extends Model
+{
+
+}
