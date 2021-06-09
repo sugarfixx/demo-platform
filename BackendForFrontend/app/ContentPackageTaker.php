e395e4e67cb4fdca66a50102ee6462b024ac8fb7
sugarfixx <ingar.torsrud@me.com>
2021-06-09 08:59:09 +0200

    Add ContentPackageTaker model

---
+++ BackendForFrontend/app/ContentPackageTaker.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 09/06/2021
+ * Time: 08:56
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContentPackageTaker extends Model
+{
+    public function contentPackage()
+    {
+        return $this->belongsTo('App\ContentPackage');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+}
