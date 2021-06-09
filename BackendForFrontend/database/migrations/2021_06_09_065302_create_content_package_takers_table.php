45dfb673472d799a17c3e12a24193d8995419c7b
sugarfixx <ingar.torsrud@me.com>
2021-06-09 08:56:20 +0200

    Add package takers migration

---
+++ BackendForFrontend/database/migrations/2021_06_09_065302_create_content_package_takers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentPackageTakersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content_package_takers', function (Blueprint $table) {
+            $table->uuid('package_id');
+            $table->foreign('package_id')->references('id')->on('content_packages');
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content_package_takers');
+    }
+}
