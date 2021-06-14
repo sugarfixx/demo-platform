dddcf95d6ccd195f42bebfcd1724adc9cad77dde
sugarfixx <ingar.torsrud@me.com>
2021-06-14 23:44:34 +0200

    Add Json Import Controller

---
+++ BackendForFrontend/app/Http/Controllers/SampleDataController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 14/06/2021
+ * Time: 23:41
+ */
+
+namespace App\Http\Controllers;
+
+
+class SampleDataController extends Controller
+{
+
+    public function importUsers()
+    {
+        //
+    }
+
+    public function importTenants()
+    {
+        //
+    }
+
+    public function importContents()
+    {
+        //
+    }
+
+
+}
