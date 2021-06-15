0a8626d4719d2de43b6c577e577d5d631d9f68d1
sugarfixx <ingar.torsrud@me.com>
2021-06-15 08:58:28 +0200

    Add route for view tenants

--- BackendForFrontend/routes/web.php
+++ BackendForFrontend/routes/web.php
@@ -19,5 +19,6 @@ $router->group(['prefix' => "sample-data"], function () use ($router) {
     $router->get('/import-users', "SampleDataController@importUsers");
     $router->get('/view-users', "SampleDataController@viewSampleUsers");
     $router->get('/import-tenants', "SampleDataController@importTenants");
+    $router->get('/view-tenants', "SampleDataController@viewSampleTenants");
     $router->get('/import-contents', "SampleDataController@importContents");
 });
