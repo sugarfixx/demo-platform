440e93397dcc2362a6ce07a5e1c2afdf636e7a6d
sugarfixx <ingar.torsrud@me.com>
2021-06-15 14:47:26 +0200

    Add bioilerplate for Tenant

---
+++ BackendForFrontend/app/Http/Controllers/TenantController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sugarfixx
+ * Date: 15/06/2021
+ * Time: 14:43
+ */
+
+namespace App\Http\Controllers;
+
+
+class TenantController extends Controller
+{
+    private $tenantId;
+
+    private $userId;
+
+    
+    public function index()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+
+    public function process()
+    {
+        //
+    }
+}
