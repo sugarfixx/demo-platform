f1bd2c9b2bc58576e170267552dea2cefb6869a5
sugarfixx <ingar.torsrud@me.com>
2021-06-30 11:01:05 +0200

    Add organizatio n service

---
+++ organization-service/src/test/java/organization/service/OrganizationServiceTest.java
@@ -0,0 +1,21 @@
+package organization.service;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class OrganizationServiceTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
